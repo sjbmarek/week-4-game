f4835f45b24648b0584816d54492187f0d4fbc4a
Susan Marek <smarek@chaskamn.com>
2017-12-10 15:35:52 -0600

    close to done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,12 +8,6 @@ var Gems = {
 		"assets/images/Gem3.png",
 		"assets/images/Gem4.png",
 		"assets/images/Gem5.png"],
-	// value: [
-	// 	(Math.floor(Math.random() * 12) + 1),
-	// 	(Math.floor(Math.random() * 12) + 1),
-	// 	(Math.floor(Math.random() * 12) + 1),
-	// 	(Math.floor(Math.random() * 12) + 1)]
-	//changed object array above into the array and method below!!
 
 	value: [0,0,0,0],
 	//define method
@@ -22,8 +16,6 @@ var Gems = {
 			this.value[i]=(Math.floor(Math.random() * 12) + 1);
 		};
 	},
-
-// };
 
 	addCrystals: function() {
 		for (var i = 0; i < this.gempic.length; i++) {
@@ -44,15 +36,6 @@ var Gems = {
 	}
 };
 
-
-
-
-
-// // this is an independant function
-// 		function price() {
-// 			(Math.floor(Math.random() * 12) + 1);
-// 		}
-
 var wins = 0;
 var losses =0;
 var targetNumber = (Math.floor(Math.random() * 101) + 19);
@@ -65,39 +48,22 @@ console.log("target number: " + targetNumber);
   var counter = 0;
   Gems.changeValues();
   console.log("value:" + Gems.value);
-
-  // // Next we create a for loop to create crystals with unique values.
-  // for (var i = 0; i < 4; i++) {
-
-  //   // Create an imageCrystal html element, add classes, add attributes
-  //   var imageCrystal = $("<img>");
-  //   imageCrystal.addClass("crystalsize");
-  //   imageCrystal.attr("src", Gems.gempic[i]);
-  //   imageCrystal.attr("data-crystalvalue", Gems.value[i]);
-
-  //   // Add the crystal to the page.
-  //   $(".crystals").append(imageCrystal);
-  // };
   Gems.addCrystals();
 
     // Click event applies to every crystal on the page.
   $(".crystalsize").on("click", function() {
+  	console.log("click happened");
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+    // Extract the value from the data attribute.
+    // THIS specifies extracting the crystalvalue of the clicked crystal.
+    // .attr("data-crystalvalue") grabs value of "data-crystalvalue" attribute.
+    // Attributes on HTML elements are strings, convert to an integer then add
    
     var crystalVal = ($(this).attr("data-crystalvalue"));
-    console.log("click value: " + crystalVal);
-    console.log("counter1: " + counter);
-
     crystalVal = parseInt(crystalVal);
-    console.log("integer value: " + crystalVal);
-    // We then add the crystalVal to the user's "counter" which is a global variable.
+    // Add the crystalVal to counter which is a global variable.
     // Every click, from every crystal adds to the global counter.
     counter += crystalVal;
-    console.log("counter2: " + counter);
     $(".score").text(counter);
 
 
@@ -107,18 +73,19 @@ console.log("target number: " + targetNumber);
       	wins++;
       	console.log("wins: " + wins);
   		$(".winnumber").text(wins);
-  		// Choose new target and gem values,reset counter and gem value
+  		// Choose new target,gem values,reset counter and gem value
   		targetNumber = (Math.floor(Math.random() * 101) + 19);
 		$(".target").text(targetNumber);
 		counter = 0;
+		$(".score").text(counter);
 		Gems.clearDiv();
   		Gems.changeValues();
   		console.log("value:" + Gems.value);
   		Gems.addCrystals();
 
-    }
+    };
 
-    else if (counter >= targetNumber) {
+    if (counter >= targetNumber) {
       	alert("Loser");
   		losses++;
   		$(".lossnumber").text(losses);
@@ -126,32 +93,22 @@ console.log("target number: " + targetNumber);
   		targetNumber = (Math.floor(Math.random() * 101) + 19);
 		$(".target").text(targetNumber);
 		counter = 0;
+		$(".score").text(counter);
 		Gems.clearDiv();
   		Gems.changeValues();
   		console.log("value:" + Gems.value);
   		Gems.addCrystals();
-    }
+    };
 
   });
 
-// defining the function
-// var marek = 63;
-// function susan(a,b) 
-// {
-
-// idx===a++;
-// b--;
-// var car = a+b;
-// }
-
-// susan(marek,3);
-
-
-
-
+// Notes
+// this is an independant function
+// 		function price() {
+// 			(Math.floor(Math.random() * 12) + 1);
+// 		}
 // getter and setter
 // js only
 // cars.forEach(function(el,i){console.log('element: ',el,',index',i})
-
-//jQuery for loop is .each
-// notes:   $.each(Array, function(idx,elmt){console.log('element: ', elmt, ',index: ', idx)});
+// jQuery for loop is .each
+// $.each(Array, function(idx,elmt){console.log('element: ', elmt, ',index: ', idx)});
