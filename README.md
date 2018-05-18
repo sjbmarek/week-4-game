795401e48a93fe057d7f2c14d237434aee47546e
Susan Marek <smarek@chaskamn.com>
2018-05-18 09:28:01 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# week-4-game
+# Crystals Collector
+
+![Crystal Collector](/Title.PNG)
+
+
+1. How the app works:
+
+   * There are four crystals displayed as buttons on the page.
+
+   * The player will be shown a random number at the start of the game.
+
+   * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+
+     * The game hides this amount until the player clicks a crystal.
+     * When they do click one, the player's score counter updates.
+
+   * The player wins if their total score matches the random number from the beginning of the game.
+
+   * The player loses if their score goes above the random number.
+
+   * The game restarts whenever the player wins or loses.
+
+   * Player wins and loses are logged.
+
+2. Tech features:
+
+   * Javascript
+   * Dynamic HTML, attributes
+   * Objects
+   * Functions
+
+3. Screenshot
+![Crystal Collector](/screenshot.PNG)
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,56 +2,56 @@
 
 
 $(document).ready(function() {
-var Gems = {
-	gempic: [
-		"assets/images/Gem1.png", 
-		"assets/images/Gem3.png",
-		"assets/images/Gem4.png",
-		"assets/images/Gem5.png"],
+  var Gems = {
+  	gempic: [
+  		"assets/images/Gem1.png", 
+  		"assets/images/Gem3.png",
+  		"assets/images/Gem4.png",
+  		"assets/images/Gem5.png"],
 
-	value: [0,0,0,0],
-	//define methods
-	changeValues: function () {
-		for (var i = 0; i < this.value.length; i++) {
-			this.value[i]=(Math.floor(Math.random() * 12) + 1);
-		};
-	},
-
-	addCrystals: function() {
-		for (var i = 0; i < this.gempic.length; i++) {
-
-    // Create an imageCrystal html element, add classes, add attributes
-    		imageCrystal = $("<img>");
-    		imageCrystal.addClass("crystalsize");
-    		imageCrystal.attr("src", this.gempic[i]);
-    		imageCrystal.attr("data-crystalvalue", this.value[i]);
-    // Add the crystal to the page.
-    		$(".crystals").append(imageCrystal);
+  	value: [0,0,0,0],
+  	//define methods
+  	changeValues: function () {
+  		for (var i = 0; i < this.value.length; i++) {
+  			this.value[i]=(Math.floor(Math.random() * 12) + 1);
   		};
+  	},
 
-	},
+  	addCrystals: function() {
+  		for (var i = 0; i < this.gempic.length; i++) {
 
+      // Create an imageCrystal html element, add classes, add attributes
+      		imageCrystal = $("<img>");
+      		imageCrystal.addClass("crystalsize");
+      		imageCrystal.attr("src", this.gempic[i]);
+      		imageCrystal.attr("data-crystalvalue", this.value[i]);
+      // Add the crystal to the page.
+      		$(".crystals").append(imageCrystal);
+    		};
 
-	clearDiv: function() {
-		$(".crystals").empty();
-	}
-};
-
-update=function() {
-	targetNumber = (Math.floor(Math.random() * 101) + 19);
-	counter = 0;
-	$(".target").text(targetNumber);
-	$(".score").text(counter);
-	$( "#name" ).fadeOut (0).fadeIn(5000);
-  	$( ".icon" ).fadeOut (0).fadeIn(5000);
-
-};
+  	},
 
 
-var imageCrystal;
-var wins = 0;
-var losses =0;
-var targetNumber;
+  	clearDiv: function() {
+  		$(".crystals").empty();
+  	}
+  };
+
+  update=function() {
+  	targetNumber = (Math.floor(Math.random() * 101) + 19);
+  	counter = 0;
+  	$(".target").text(targetNumber);
+  	$(".score").text(counter);
+  	$( "#name" ).fadeOut (0).fadeIn(5000);
+    $( ".icon" ).fadeOut (0).fadeIn(5000);
+
+  };
+
+
+  var imageCrystal;
+  var wins = 0;
+  var losses =0;
+  var targetNumber;
 
 //This is the start of the action.
 
@@ -111,15 +111,5 @@ var targetNumber;
 
   });
 
-  });
+});
 
-// Notes
-// this is an independant function
-// something = 	function price() {
-// 			(Math.floor(Math.random() * 12) + 1);
-// 		}
-// getter and setter
-// js only
-// cars.forEach(function(el,i){console.log('element: ',el,',index',i})
-// jQuery for loop is .each
-// $.each(Array, function(idx,elmt){console.log('element: ', elmt, ',index: ', idx)});
